6a66f24c196584df4df6ab568fc21119d7fb7963
toby lorne <toby@toby.codes>
2021-04-10 14:45:29 +0200

    epubs: can read a book
    looks really bad but it works
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- epubs.rb
+++ epubs.rb
@@ -36,10 +36,36 @@ class Hivemind < Sinatra::Base
 
     erb :"epubs/show"
   end
+
+  get %r(/epubs/(?<epub_id>\d+)/href/(?<href>.*)) do
+    @epub = EPub.find(id: params[:epub_id])
+    raise Sinatra::NotFound if @epub.nil?
+
+    @item = @epub.parsed.item_by_href(params[:href])
+
+    if @item.media_type =~ /image/
+      content_type @item.media_type
+      @item.content
+    else
+      erb :"epubs/read"
+    end
+  end
 end
 
 class EPub < Sequel::Model
   def parsed
     GEPUB::Book.parse StringIO.new(self.epub.to_s)
+  end
+
+  def href_path(href)
+    "/epubs/#{id}/href/#{href}"
+  end
+
+  def prev_href(item)
+    parsed.items.values.take_while { |i| i.id != item }.last&.href
+  end
+
+  def next_href(item)
+    parsed.items.values.drop_while { |i| i.id != item }.drop(1).first&.href
   end
 end
