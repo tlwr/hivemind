743367618bb348f01bf1083dd3019b3e2223a30a
toby lorne <toby@toby.codes>
2021-04-16 11:03:20 +0200

    spec: use robots.txt for static test
    style.css does not exist on fresh clones
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- spec/static_spec.rb
+++ spec/static_spec.rb
@@ -1,8 +1,8 @@
 require_relative "spec_helper"
 
 RSpec.describe "static" do
-  it "serves css" do
-    get "/style.css"
+  it "serves a static text file" do
+    get "/robots.txt"
     expect(last_response).to be_ok
   end
 end
