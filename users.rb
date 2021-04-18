1e1e36d07f91c080de00edab89ded2b63c29547b
toby lorne <toby@toby.codes>
2021-04-18 17:30:53 +0200

    users: add profile path and link from epub show
    Signed-off-by: toby lorne <toby@toby.codes>

--- users.rb
+++ users.rb
@@ -5,4 +5,16 @@ class Hivemind < Sinatra::Base
     @user = current_user
     erb :"users/show"
   end
+
+  get %r(/@(?<username>[^/]+)) do
+    @user = User.find(username: params[:username])
+    raise Sinatra::NotFound if @user.nil?
+    erb :"users/show"
+  end
+end
+
+class User < Sequel::Model
+  def show_path
+    "/@#{username}"
+  end
 end
