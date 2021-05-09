d485015cd0203b0d5ab5f090778ff6f04d6aa12a
toby lorne <toby@toby.codes>
2021-05-09 11:48:30 +0200

    bookmarks: can delete a bookmark
    Signed-off-by: toby lorne <toby@toby.codes>

--- bookmarks.rb
+++ bookmarks.rb
@@ -5,4 +5,9 @@ class Hivemind < Sinatra::Base
     @bookmarks = current_user.epub_bookmarks
     erb :"bookmarks/index"
   end
+
+  post "/bookmarks/:id/delete" do
+    current_user.epub_bookmarks.find(id: params[:id])&.first&.delete
+    redirect "/bookmarks"
+  end
 end
