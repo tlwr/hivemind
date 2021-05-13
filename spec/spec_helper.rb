6af1ad7671b5d7749d4c28e83659e01443d6b9d5
toby lorne <toby@toby.codes>
2021-05-13 16:03:43 +0200

    spec: add login and token helpers
    Signed-off-by: toby lorne <toby@toby.codes>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,25 @@ end
 def fixture_path(path)
   File.expand_path(File.join(File.dirname(__FILE__), "fixtures", path))
 end
+
+def token_from_current_page
+  last_response.body.match(/authenticity_token.+value="([^"]+)"/)[1]
+end
+
+def login
+  get "/login"
+
+  post "/login", {
+    "username" => "test",
+    "password" => "test-user-password",
+    "authenticity_token" => token_from_current_page,
+  }
+
+  expect(last_response).to be_redirect
+  follow_redirect!
+
+  expect(last_response).to be_redirect
+  follow_redirect!
+
+  expect(last_response).to be_ok
+end
